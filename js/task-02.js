52aa2ec68f11d690d3573a0b257b3c5f099c573e
yura-prots <yuraprots@gmail.com>
2023-12-06 22:35:25 +0200

    task-02 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const itemList = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.className = "item";
+
+  return itemEl;
+});
+
+document.querySelector("#ingredients").append(...itemList);
