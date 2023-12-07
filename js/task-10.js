88d5d57f92e4fc8d0ad8821cb1c6f731f7040e09
yura-prots <yuraprots@gmail.com>
2023-12-08 00:35:17 +0200

    task-10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const parentBox = document.getElementById("boxes");
+const numberOfEl = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+function createBoxes(amount) {
+  const elementsArray = [];
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    boxSize += 10;
+
+    const divEl = document.createElement("div");
+    divEl.style.width = `${boxSize}px`;
+    divEl.style.height = `${boxSize}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+
+    elementsArray.push(divEl);
+  }
+
+  return parentBox.append(...elementsArray);
+}
+
+createBtn.addEventListener("click", () => {
+  createBoxes(numberOfEl.value);
+});
+
+destroyBtn.addEventListener("click", () => {
+  parentBox.innerHTML = "";
+});
