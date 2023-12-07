675b8f809719621880d639b64c3f9c3e07142a7a
yura-prots <yuraprots@gmail.com>
2023-12-07 20:45:55 +0200

    task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const spanEl = bodyEl.querySelector(".color");
+const buttonEl = bodyEl.querySelector(".change-color");
+
+buttonEl.addEventListener("click", () => {
+  let randomColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = `- ${randomColor}`;
+});
