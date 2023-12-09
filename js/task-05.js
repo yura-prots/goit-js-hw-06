9fa5061cc82c9e3d9a2ce0e43792d811f00da5d9
yura-prots <yuraprots@gmail.com>
2023-12-09 16:56:22 +0200

    task-05 corrected

--- js/task-05.js
+++ js/task-05.js
@@ -3,5 +3,9 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", (e) => {
-  nameOutput.textContent = e.currentTarget.value;
+  if (!e.currentTarget.value) {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = e.currentTarget.value;
+  }
 });
