f3715f85b773316ec656823c5679d7181cbe68ec
yura-prots <yuraprots@gmail.com>
2023-12-09 16:50:10 +0200

    task-06 corrected

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,14 @@
 const validationInput = document.getElementById("validation-input");
 
 validationInput.addEventListener("blur", (e) => {
-  const validLength = validationInput.dataset.length;
-  let stringLength = e.currentTarget.value.length;
+  const validLength = Number(validationInput.dataset.length);
+  let stringLength = Number(e.currentTarget.value.length);
 
-  if (stringLength < validLength) {
-    validationInput.classList.add("invalid");
-  } else if (stringLength >= validLength) {
-    validationInput.classList.remove("invalid");
+  if (stringLength === validLength) {
     validationInput.classList.add("valid");
+    validationInput.classList.remove("invalid");
+  } else {
+    validationInput.classList.remove("valid");
+    validationInput.classList.add("invalid");
   }
 });
